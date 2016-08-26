78371cc2dc916b314d9f4b55a3e99e91cc3001cc
“chinx <c54948054@126.com>
2016-08-26 10:12:07 +0800

    Point type str

--- models/point.go
+++ models/point.go
@@ -20,7 +20,7 @@ const (
 type Point struct {
 	ID         int64      `json:"id" gorm:"primary_key"`
 	PID        int64      `json:"pid" gorm:"type:int;not null;index"`
-	Type       string     `json:"type" binding:"In(Star,Check,End)" gorm:"size:20"`
+	Type       string     `json:"type" binding:"In(Start,Check,End)" gorm:"size:20"`
 	Triggers   string     `json:"triggers" gorm:"varchar(255)"` // eg : "a,b,c"
 	Conditions string     `json:"conditions" gorm:"size:255"`   // eg : "a,b,c"
 	Status     uint       `json:"Status" gorm:"type:int;default:0"`
